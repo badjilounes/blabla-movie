b672c8348e3eb7cfd1a361076080e1a34a708588
badjilounes <lounes@silk-digital.com>
2018-10-01 01:31:09 +0200

    # Missing unversioned files

---
+++ src/app/pages/movie-detail/movie-detail.component.ts
@@ -0,0 +1,24 @@
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {MovieService} from '../../service/movie/movie.service';
+import {ActivatedRoute} from '@angular/router';
+import {DetailInput} from '../../model/detail.input';
+import {Observable} from 'rxjs/index';
+
+@Component({
+  selector: 'app-movie-detail',
+  templateUrl: './movie-detail.component.html',
+  styleUrls: ['./movie-detail.component.scss']
+})
+export class MovieDetailComponent implements AfterViewInit {
+
+  detailInfo: Observable<any> = undefined;
+
+  constructor(private movieSrv: MovieService, private route: ActivatedRoute, private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    let detail: DetailInput = {i: this.route.snapshot.params.id};
+    this.detailInfo = this.movieSrv.getMovieDetail(detail);
+    this.cdr.markForCheck()
+  }
+
+}
